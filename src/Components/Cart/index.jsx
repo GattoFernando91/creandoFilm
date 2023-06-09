c18951851e5005b1bcdd7fffc3bb59fd4bab9cae
Fernando Gatto <gatto91@gmail.com>
2023-06-09 12:16:23 +0200

    finalizado, solo falta agregarle la estetica

--- src/Components/Cart/index.jsx
+++ src/Components/Cart/index.jsx
@@ -1,49 +1,133 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useCartContext } from "../../context/cartContext";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ItemCart } from "../ItemCart";
-import { addDoc, collection, getFirestore} from 'firebase/firestore'
-import './cart.css'
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
+import './cart.css';
+
 
 const Cart = () => {
-    const { cart, totalPrice } = useCartContext();
+    const navigate = useNavigate();
+    const { cart, totalPrice, clearCart } = useCartContext();
+    const [showModal, setShowModal] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [address, setAddress] = useState("");
+    const [formValid, setFormValid] = useState(false);
+    const [purchaseSuccess, setPurchaseSuccess] = useState(false);
+    const [countdown, setCountdown] = useState(5);
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    };
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePhoneChange = (e) => {
+        setPhone(e.target.value);
+    };
+
+    const handleAddressChange = (e) => {
+        setAddress(e.target.value);
+    };
+
+    const validateForm = useCallback(() => {
+        if (name.trim() !== "" && email.trim() !== "" && phone.trim() !== "" && address.trim() !== "") {
+            setFormValid(true);
+        } else {
+            setFormValid(false);
+        }
+    }, [name, email, phone, address]);
+
+    const openModal = () => {
+        setShowModal(true);
+    };
+
+    const closeModal = () => {
+        setShowModal(false);
+    };
 
     const order = {
         buyer: {
-            name: 'Fernando',
-            email: 'gatto91@gmail.com',
-            phone: '1565869384',
-            address: 'Ravignani 1872'
+            name,
+            email,
+            phone,
+            address
         },
-        items: cart.map(product => ({ id: product.id , title: product.id, price: product.price, quantity: product.quantity })),
+        items: cart.map(product => ({ id: product.id, title: product.id, price: product.price, quantity: product.quantity })),
         total: totalPrice,
-    }
+    };
 
     const handleClick = () => {
-        const db = getFirestore()
-        const ordersCollection = collection(db, 'orders')
+        const db = getFirestore();
+        const ordersCollection = collection(db, 'orders');
         addDoc(ordersCollection, order)
-            .then(({ id }) => console.log(id))
-    }
+            .then(({ id }) => {
+                console.log(id);
+                setPurchaseSuccess(true);
+                const countdownInterval = setInterval(() => {
+                    setCountdown((prevCountdown) => prevCountdown - 1);
+                }, 1000);
+
+                setTimeout(() => {
+                    clearInterval(countdownInterval);
+                    clearCart();
+                    navigate('/');
+                }, 5000); // Redirect after 5 seconds
+            });
+    };
+
+    useEffect(() => {
+        validateForm();
+    }, [name, email, phone, address, validateForm]);
 
     if (cart.length === 0) {
         return (
             <>
-            <p>No hay servicios en el carrito.</p>
-            <Link to='/Servicios'>Comprar Servicios</Link>
+                <p>No hay servicios en el carrito.</p>
+                <Link to='/Servicios'>Comprar Servicios</Link>
             </>
-        )
+        );
     }
+
 
     return (
         <>
-        {
-            cart.map(product => <ItemCart key={product.id} product={product} />)
-        }
-        <p>Total: ${totalPrice}</p>
-        <button onClick={handleClick}>Finalizar Compra</button>
+            {cart.map(product => <ItemCart key={product.id} product={product} />)}
+            {!showModal && !purchaseSuccess && (
+                <>
+                    <p>Total: ${totalPrice}</p>
+                    <button onClick={openModal}>Completar Datos de Compra</button>
+                </>
+            )}
+    
+            {showModal && !purchaseSuccess && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <span className="close" onClick={closeModal}>&times;</span>
+                        <form>
+                            <input type="text" placeholder="Nombre" value={name} onChange={handleNameChange} onBlur={validateForm} required />
+                            <input type="email" placeholder="Email" value={email} onChange={handleEmailChange} onBlur={validateForm} required />
+                            <input type="tel" placeholder="Teléfono" value={phone} onChange={handlePhoneChange} onBlur={validateForm} required />
+                            <input type="text" placeholder="Dirección" value={address} onChange={handleAddressChange} onBlur={validateForm} required />
+                        </form>
+                        <button onClick={handleClick} disabled={!formValid}>Finalizar Compra</button>
+                    </div>
+                </div>
+            )}
+    
+            {purchaseSuccess && (
+                <>
+                    <h2>Compra Realizada con Éxito!</h2>
+                    <p>En breve recibira un mail con la informacion.</p>
+                    <p>Será redirigido al Inicio en {countdown} segundos.</p>
+                </>
+            )}
         </>
-    )
+    );
 }
 
-export { Cart }
+export { Cart };
